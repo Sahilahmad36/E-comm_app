daf81a61f8d50097f7fdbff1def32caaba345f23
Sahil <125720378+Sahilahmad36@users.noreply.github.com>
2023-07-25 17:36:21 +0530

    Deployed website

--- back-end/config.js
+++ back-end/config.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://0.0.0.0:27017/employee");
+// mongoose.connect("mongodb://0.0.0.0:27017/employee");
+mongoose.connect("mongodb+srv://ahmadsahil3600:root1234@cluster0.wjahyro.mongodb.net/");
 
